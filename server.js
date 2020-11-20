66e8c397507badc7eaf0d76af00bc87fb6e02afd
Nestor Martinez <darkalex595@gmail.com>
2020-11-20 01:11:04 -0600

    Se añadio el server de express

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',  function (req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get('/user', function (req, res){
+    res.sendFile(path.join(__dirname, "infouser.html"));
+})
+
+app.listen(PORT, function(){
+    console.log("Server Up on Port 3000")
+})
+
